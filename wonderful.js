5185ff3a32db422b3eabfbe6dc2aa7c95017619a
Farhan Syed <nearhan@gmail.com>
2016-11-12 15:59:46 -0500

    added basic things

--- wonderful.js
+++ wonderful.js
@@ -7,6 +7,36 @@ function onTwitter() {
 
 function doReplace() {
     console.log("do replace here");
+    multipyFollowers(100000)
 }
 
 onTwitter();
+
+function multipyFollowers(n) {
+   var htmlCollection = document.getElementsByClassName("ProfileNav-value")
+   // get followers
+   var followers = htmlCollection[2]
+   var value = humanizeNumber(followers.innerHTML)
+   followers.innerHTML = multipyAndConvert(value, n)
+}
+
+function multipyAndConvert(value, n ) {
+    console.log(value, n);
+    console.log(typeof(value))
+    var _val = (value * Math.random() * n * n)
+    console.log(_val)
+    return _val
+}
+
+function humanizeNumber(value) {
+    if (value.includes("M") || value.includes("K")) {
+        value = parseInt(value.split("M")[0])
+        return value
+    }
+    return parseInt(value)
+
+}
+
+
+
+
