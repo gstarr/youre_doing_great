c549dde058a1ae9a94a42ad511de332188feebb1
Coleman McFarland <coleman.mcfarland@deciphertechstudios.com>
2016-11-11 22:11:01 -0500

    Only run extension on Twitter.com

--- wonderful.js
+++ wonderful.js
@@ -1,1 +1,12 @@
-//alert("Hello Fuck");
+
+function onTwitter() {
+  if (document.URL.includes("twitter.com")) {
+      doReplace();
+  }
+}
+
+function doReplace() {
+    console.log("do replace here");
+}
+
+onTwitter();
